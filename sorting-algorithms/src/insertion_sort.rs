31bfdb5abe11ad6188fd44730adaaf55471096b7
Jean LELIEVRE <Jean.LELIEVRE@supinfo.com>
2017-06-20 13:15:57 +0200

    add new insertion sort module

---
+++ sorting-algorithms/src/insertion_sort.rs
@@ -0,0 +1,15 @@
+pub fn iterate_over_insertion_sort(
+    array: &mut [u8; 50], 
+    a: &mut usize,
+    b: &mut usize,
+)
+{
+    *b = *a;
+
+    while *b > 0 && array[*b - 1] > array[*b] {
+        array.swap(*b, *b - 1);
+        *b -= 1;
+    }
+
+    *a += 1;
+}
