a37d48cb82c59442f6e578dc9828b078db8e5e33
Jean LELIEVRE <Jean.LELIEVRE@supinfo.com>
2017-06-27 13:49:07 +0200

    define the bubble sort method

--- sorting-algorithms/src/bubble_sort.rs
+++ sorting-algorithms/src/bubble_sort.rs
@@ -1,5 +1,18 @@
 //! Provides the bubble sort feature
 
 /// Triggers one step of the bubble sort
-pub fn iterate_over_bubble_sort() {
+pub fn iterate_over_bubble_sort(
+    array: &mut [u8],
+    index: &mut usize,
+    swapped: &mut bool,
+) {
+    if array[*index - 1] > array[*index] {
+        array.swap(
+            *index - 1,
+            *index,
+        );
+        *swapped = true;
+    }
+
+    *index += 1;
 }
